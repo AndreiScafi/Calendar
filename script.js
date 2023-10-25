9b6a716a184ff85edfe41d672428668f2fd0e30e
Andrei Scafi <andreiscafi@hotmail.com>
2023-10-25 10:10:58 -0300

    Getting the days of the month with Javascript.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const dateH1 = document.querySelector('.date h1');
 const dateP = document.querySelector('.date p');
+const daysHMTL = document.querySelector('.days');
 
 const date = new Date();
+
+const monthDays = daysHMTL;
+
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
 const months = [
     "January",
@@ -22,3 +27,13 @@ dateH1.innerHTML = months[date.getMonth()];
 
 dateP.innerHTML = date.toDateString();
 
+let days = "";
+
+for (let i = 1; i <= lastDay; i++) {
+
+    days += `<div>${i}</div>`;
+}
+
+monthDays.innerHTML = days;
+
+console.log(lastDay)
