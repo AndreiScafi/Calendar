ab1923244d8ebdea1a18f6b192d0544565a2a6ae
Andrei Scafi <andreiscafi@hotmail.com>
2023-10-25 10:33:05 -0300

    adding prev and next days and highlight today's date with Javascript.

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ const daysHMTL = document.querySelector('.days');
 
 const date = new Date();
 
+date.setDate(1);
+
 const monthDays = daysHMTL;
 
 const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+const firstDayIndex = date.getDay();
+
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+
+const nextDays = 7 - lastDayIndex - 1;
 
 const months = [
     "January",
@@ -29,11 +39,22 @@ dateP.innerHTML = date.toDateString();
 
 let days = "";
 
-for (let i = 1; i <= lastDay; i++) {
+for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class = "prev-date">${prevLastDay - x + 1}</div>`;
+}
 
-    days += `<div>${i}</div>`;
+
+for (let i = 1; i <= lastDay; i++) {
+    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+        days += `<div class="today">${i}</div>`;
+    } else {
+        days += `<div>${i}</div>`;
+    }
+}
+
+for (let y = 1; y <= nextDays; y++) {
+    days += `<div class="next-date">${y}</div>`
+
 }
 
 monthDays.innerHTML = days;
-
-console.log(lastDay)
